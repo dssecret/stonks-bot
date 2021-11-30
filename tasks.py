6eb04aa6bceeb7c3c66bf10a0662542bb69d2b6c
dssecret <dsstudios.spam@gmail.com>
2021-11-30 13:49:58 -0800

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -14,7 +14,7 @@ class Tasks(commands.Cog):
         self.bollinger.start()
         
     @tasks.loop(minutes=1)
-    async def bollinger(self):
+    async def bollinger(self, window=20, std=1):
         stocks = [
             'TSB',
             'TCB',
@@ -51,4 +51,40 @@ class Tasks(commands.Cog):
         ]
         
         for stock in stocks:
-            print(requests.get(f'https://tornsy.com/api/{stock.lower()}').json())
+            data = requests.get(f'https://tornsy.com/api/{stock.lower()}').json()
+            
+            prices = pandas.DataFrame(columns=['price', 'timestamp'])
+            rows = 0
+            
+            for row in data['data'].reverse():
+                if row >= window:
+                    break
+                
+                prices.loc[-1] = [data[1], data[0]]
+                rows += 1
+            
+            prices['ma'] = prices.price.rolling(window=window).mean()
+            prices['uma'] = prices.price.rolling(window=window).mean() + (prices.price.rolling(window=window).std() * std)
+            prices['lma'] = prices.price.rolling(window=window).mean() - (prices.price.rolling(window=window).std() * std)
+            
+            if prices[-1]['uma'] < prices[-1]['price']:
+                embed = discord.Embed()
+                embed.title = f'BUY Order on {stock}'
+                embed.description = f'Buy order on {stock} due to Bollinger Bands.'
+                embed.add_field(name='Current Price', value=prices[-1]['price'])
+                embed.add_field(name='Lower Moving Average', value=prices[-1]['lma'])
+                embed.add_field(name='Upper Moving Average', value=prices[-1]['uma'])
+                
+                channel = discord.utils.get(self.bot.guilds[0].channels, name='stonks')
+                await channel.send(embed=embed)
+            elif prices[-1]['lma'] > prices[-1]['price']:
+                embed = discord.Embed()
+                embed.title = f'SELL Order on {stock}'
+                embed.description = f'Sell order on {stock} due to Bollinger Bands.'
+                embed.add_field(name='Current Price', value=prices[-1]['price'])
+                embed.add_field(name='Lower Moving Average', value=prices[-1]['lma'])
+                embed.add_field(name='Upper Moving Average', value=prices[-1]['uma'])
+                
+                channel = discord.utils.get(self.bot.guilds[0].channels, name='stonks')
+                await channel.send(embed=embed)
+            
